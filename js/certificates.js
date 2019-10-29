e98cf0279a57ecda66fa39d26492440d9cac92c0
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-10-29 13:24:32 +0530

    certs

--- js/certificates.js
+++ js/certificates.js
@@ -1,12 +1,13 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDiQTQsDF09cFCDco8BAm6GUUG8EUpesTs",
-    authDomain: "mstc-certificates.firebaseapp.com",
-    databaseURL: "https://mstc-certificates.firebaseio.com",
-    projectId: "mstc-certificates",
-    storageBucket: "mstc-certificates.appspot.com",
-    messagingSenderId: "786275531768",
-    appId: "1:786275531768:web:46474baf1688bf88"
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyDSMQzs-H_0S8KVJ_4Lafb4OK4oN_41nGc",
+    authDomain: "mstc-certificates-4d37e.firebaseapp.com",
+    databaseURL: "https://mstc-certificates-4d37e.firebaseio.com",
+    projectId: "mstc-certificates-4d37e",
+    storageBucket: "mstc-certificates-4d37e.appspot.com",
+    messagingSenderId: "809628938657",
+    appId: "1:809628938657:web:0462df4448b01842cb9be0",
+    measurementId: "G-ERM4DF0K0C"
+  };
 var storage;
 
 
@@ -16,6 +17,8 @@ $(document).ready(function () {
     console.log("Firebase Initialised.")
 });
 
+
+// EHcerts
 function dod() {
     document.getElementById("progress").style.visibility='visible';
     var storageRef = storage.ref();
@@ -29,7 +32,7 @@ function dod() {
         document.getElementById("progress").style.visibility='hidden';
         return;
     }
-    var filepath = 'competitive-coding//' + regno + '.pdf';
+    var filepath = 'Ethical//' + regno + '.pdf';
     var pathReference = storage.ref(filepath);
 
     storageRef.child(filepath).getDownloadURL().then(function (url) {
@@ -51,3 +54,82 @@ function dod() {
 
 
 }
+
+//BCAICerts
+function dod1() {
+    document.getElementById("progress").style.visibility='visible';
+    var storageRef = storage.ref();
+    var regno = document.getElementById('regid').value;
+    if(regno.length <5){
+        console.log(regno.length);
+        document.getElementById("datalink").innerHTML = "";
+        document.getElementById("datalink").style.visibility='hidden';
+        document.getElementById("errorlink").innerHTML="Please enter your email correctly";
+        document.getElementById("errorlink").style.visibility='visible';
+        document.getElementById("progress").style.visibility='hidden';
+        return;
+    }
+    var filepath = 'BC-AI//' + regno + '.pdf';
+    var pathReference = storage.ref(filepath);
+
+    storageRef.child(filepath).getDownloadURL().then(function (url) {
+        document.getElementById("datalink").innerHTML = "Download Certificate";
+        document.getElementById("datalink").href = url;
+        document.getElementById("datalink").style.visibility='visible';
+        document.getElementById("errorlink").style.visibility='hidden';
+        document.getElementById("progress").style.visibility='hidden';
+        console.log("FOUND IT BABY!");
+    }).catch(function (error) {
+        document.getElementById("datalink").innerHTML = "";
+        document.getElementById("datalink").style.visibility='hidden';
+        document.getElementById("errorlink").innerHTML="There was no certificate with this email id, please try again.";
+        document.getElementById("errorlink").style.visibility='visible';
+        document.getElementById("progress").style.visibility='hidden';
+        console.log("OOPS!");
+    });
+
+
+
+}
+
+
+//ADCerts
+function dod2() {
+    document.getElementById("progress").style.visibility='visible';
+    var storageRef = storage.ref();
+    var regno = document.getElementById('regid').value;
+    if(regno.length <5){
+        console.log(regno.length);
+        document.getElementById("datalink").innerHTML = "";
+        document.getElementById("datalink").style.visibility='hidden';
+        document.getElementById("errorlink").innerHTML="Please enter your email correctly";
+        document.getElementById("errorlink").style.visibility='visible';
+        document.getElementById("progress").style.visibility='hidden';
+        return;
+    }
+    var filepath = 'DevOps//' + regno + '.pdf';
+    var pathReference = storage.ref(filepath);
+
+    storageRef.child(filepath).getDownloadURL().then(function (url) {
+        document.getElementById("datalink").innerHTML = "Download Certificate";
+        document.getElementById("datalink").href = url;
+        document.getElementById("datalink").style.visibility='visible';
+        document.getElementById("errorlink").style.visibility='hidden';
+        document.getElementById("progress").style.visibility='hidden';
+        console.log("FOUND IT BABY!");
+    }).catch(function (error) {
+        document.getElementById("datalink").innerHTML = "";
+        document.getElementById("datalink").style.visibility='hidden';
+        document.getElementById("errorlink").innerHTML="There was no certificate with this email id, please try again.";
+        document.getElementById("errorlink").style.visibility='visible';
+        document.getElementById("progress").style.visibility='hidden';
+        console.log("OOPS!");
+    });
+
+
+
+}
+
+
+
+
